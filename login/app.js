db3e535f931d14e94a4fae5b475bd1daee700cc6
Logon Aluno <logonrmlocal@fiap.com.br>
2023-08-29 09:41:45 -0300

    Firebase

---
+++ login/app.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDftzD6r6egaLRPgG_lUPZa9lSSNqVlcX8",
+  authDomain: "projeto2miateste-ddbf2.firebaseapp.com",
+  projectId: "projeto2miateste-ddbf2",
+  storageBucket: "projeto2miateste-ddbf2.appspot.com",
+  messagingSenderId: "1080394016362",
+  appId: "1:1080394016362:web:4884a377629ee2b9954c13",
+};

0c15a6aaf479a0f3820eb7415b57820e285aa713
Logon Aluno <logonrmlocal@fiap.com.br>
2023-08-29 13:28:07 -0300

    Firebase

--- login/app.js
+++ login/app.js
@@ -6,3 +6,26 @@ const firebaseConfig = {
   messagingSenderId: "1080394016362",
   appId: "1:1080394016362:web:4884a377629ee2b9954c13",
 };
+
+firebase.initializeApp(firebaseConfig);
+
+const emailField = document.getElementById("email");
+const passwordField = document.getElementById("password");
+const loginButton = document.getElementById("loginButton");
+
+loginButton.addEventListener("click", () => {
+  const email = emailField.value;
+  const password = passwordField.value;
+
+  firebase
+    .auth()
+    .signInWithEmailAndPassword(email, password)
+    .then((useCredential) => {
+      const user = useCredential.user;
+      console.log("Usuário logado: ", user);
+    })
+    .catch((error) => {
+      const errorMessage = error.message;
+      console.error("Erro de autenticação: ", errorMessage);
+    });
+});

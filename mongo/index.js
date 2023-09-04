16ac5c4586e8673429ef4dde2c6800661d0d30de
Logon Aluno <logonrmlocal@fiap.com.br>
2023-09-04 10:08:43 -0300

    Mongoose

---
+++ mongo/index.js
@@ -0,0 +1,18 @@
+// *No MongoDB Compass*
+// use test
+// db.createCollection("exemplo")
+
+const mongoose = require("mongoose");
+
+mongoose.connect("mongodb://localhost:27017/test", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const db = mongoose.connection;
+
+db.on("error", console.error.bind(console, "connection error: "));
+
+db.on("open", function () {
+  console.log("Estamos conectados ao banco");
+});

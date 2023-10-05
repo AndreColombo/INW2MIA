312b3dd52f22fa456bd93b5df771c80323f3a08b
André <rm15019@fiap.com.br>
2023-10-05 09:38:59 -0300

    CALCULAR MÉDIAS

--- Média/media.js
+++ Média/media.js
@@ -1,19 +1,19 @@
 const leia = require("prompt-sync")();
 
-const at = parseInt(leia("Nota da AT: "));
-const ac = parseInt(leia("Nota da AC: "));
-const agh = parseInt(leia("Nota da AGH: "));
+const at = parseInt(leia("Digite a nota da AT: "));
+const ac = parseInt(leia("Digite a nota da AC: "));
+const agh = parseInt(leia("Digite a nota da AGH: "));
 
 const mediaTri = (at * 5 + ac * 3 + agh * 2) / 10;
 
-console.log("Média Trimestral: " + mediaTri);
+console.log("Sua média Trimestral é: " + mediaTri);
 
 console.log("-----------------------");
 
-const triUm = parseInt(leia("Média do 1ºTri: "));
-const triDois = parseInt(leia("Média do 2ºTri: "));
-const triTres = parseInt(leia("Média do 3ºTri: "));
+const triUm = parseInt(leia("Digite a média do 1ºTri: "));
+const triDois = parseInt(leia("Digite a média do 2ºTri: "));
+const triTres = parseInt(leia("Digite a média do 3ºTri: "));
 
 const mediaAno = (triUm * 1 + triDois * 2 + triTres * 3) / 6;
 
-console.log("Média Anual: " + mediaAno);
+console.log("Sua média Anual é: " + mediaAno);
